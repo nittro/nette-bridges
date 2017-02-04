ad5f9fc5f779a574f14755ed4cff2ccb3c824339
danik <me@subsonic.cz>
2017-02-04 15:49:45 +0100

    n:flashTarget and some fixes & cleanups

--- src/Bridges/NittroLatte/NittroMacros.php
+++ src/Bridges/NittroLatte/NittroMacros.php
@@ -1,18 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: danik
- * Date: 26/02/16
- * Time: 13:09
- */
 
 namespace Nittro\Bridges\NittroLatte;
-use Latte\CompileException;
-use Latte\Compiler,
-    Latte\Macros\MacroSet;
-use Latte\MacroNode;
-use Latte\MacroTokens;
-use Latte\PhpWriter;
+
+use Latte\Macros\MacroSet,
+    Latte\Compiler,
+    Latte\CompileException,
+    Latte\MacroNode,
+    Latte\PhpWriter;
 
 
 class NittroMacros extends MacroSet {
@@ -20,13 +14,43 @@ class NittroMacros extends MacroSet {
     public static function install(Compiler $compiler)
     {
         $me = new static($compiler);
-        $me->addMacro('snippetId', 'echo %escape($_control->getSnippetId(%node.word))');
-        $me->addMacro('param', 'echo %escape($_control->getParameterId(%node.word))');
-        $me->addMacro('dynamic', null, null, 'echo Nette\Utils\Html::el()->setClass("nittro-snippet-container")->data("dynamic-mask", $_control->getSnippetId(%node.word))->attributes()');
+        $me->addMacro('snippetId', 'echo %escape($this->global->snippetDriver->getHtmlId(%node.word))');
+        $me->addMacro('param', 'echo %escape($this->global->uiControl->getParameterId(%node.word))');
+        $me->addMacro('flashTarget', null, null, [$me, 'macroFlashTarget']);
+        $me->addMacro('dynamic', null, null, [$me, 'macroDynamic']);
         $me->addMacro('errors', [$me, 'macroErrors'], [$me, 'macroErrorsEnd'], null, self::AUTO_EMPTY);
         $me->addMacro('formErrors', [$me, 'macroErrors'], [$me, 'macroErrorsEnd'], null, self::AUTO_EMPTY);
     }
 
+
+    public function macroFlashTarget(MacroNode $node, PhpWriter $writer) {
+        if (!$node->prefix || $node->prefix !== MacroNode::PREFIX_NONE) {
+            throw new CompileException('Unknown macro ' . $node->getNotation() . ', did you mean n:' . $node->name . '?');
+        } else if (isset($node->htmlNode->attrs['id'])) {
+            throw new CompileException('Cannot combine HTML attribute id with ' . $node->getNotation());
+        }
+
+        $node->attrCode = $writer->write('<?php echo \' id="flash-\' . htmlSpecialChars($this->global->uiControl->getParameterId(\'messages\')) . \'"\' ?>');
+    }
+
+
+    public function macroDynamic(MacroNode $node, PhpWriter $writer) {
+        if (!$node->prefix || $node->prefix !== MacroNode::PREFIX_NONE) {
+            throw new CompileException('Unknown macro ' . $node->getNotation() . ', did you mean n:' . $node->name . '?');
+        }
+
+        $attrCode = 'echo \' data-dynamic-container="\' . htmlSpecialChars($this->global->snippetDriver->getHtmlId(%node.word)) . \'"\'';
+
+        if (!empty($node->htmlNode->attrs['class'])) {
+            if (!preg_match('/(?:^|\s)nittro-snippet-container(?:\s|$)/', $node->htmlNode->attrs['class'])) {
+                throw new CompileException('Dynamic container specifying the "class" attribute must include the "nittro-snippet-container" class');
+            }
+        } else {
+            $attrCode .= ' . \' class="nittro-snippet-container"\'';
+        }
+
+        $node->attrCode = $writer->write("<?php $attrCode ?>");
+    }
 
     public function macroErrors(MacroNode $node, PhpWriter $writer) {
         if ($node->modifiers) {
@@ -49,11 +73,11 @@ class NittroMacros extends MacroSet {
 
         if ($node->name === 'formErrors') {
             $prefix = $writer->write(
-                '$_form = ' . ($name && $name[0] === '$' ? 'is_object(%0.word) ? %0.word : ' : '')
+                '$_tmp = ' . ($name && $name[0] === '$' ? 'is_object(%0.word) ? %0.word : ' : '')
                 . ($name ? '$this->global->uiControl[%0.word];' : 'end($this->global->formsStack);')
-                . ' $_el = Nette\Utils\Html::el(%1.var)->setId($_form->getElementPrototype()->id . \'-errors\')'
+                . ' $_tmp2 = Nette\Utils\Html::el(%1.var)->setId($_tmp->getElementPrototype()->id . \'-errors\')'
                 . ($node->tokenizer->isNext() ? '->addAttributes(%node.array);' : ';')
-                . ' foreach($_form->getOwnErrors() as $_e) $_el->create(%2.var)->setText($_e)',
+                . ' foreach($_tmp->getOwnErrors() as $_e) $_tmp2->create(%2.var)->setText($_e)',
                 $name,
                 $tagName,
                 $childName
@@ -64,11 +88,11 @@ class NittroMacros extends MacroSet {
             }
 
             $prefix = $writer->write(
-                '$_input = ' . ($name[0] === '$' ? 'is_object(%0.word) ? %0.word : ' : '')
+                '$_tmp = ' . ($name[0] === '$' ? 'is_object(%0.word) ? %0.word : ' : '')
                 . 'end($this->global->formsStack)[%0.word];'
-                . ' $_el = Nette\Utils\Html::el(%1.var)->setId($_input->getHtmlId() . \'-errors\')'
+                . ' $_tmp2 = Nette\Utils\Html::el(%1.var)->setId($_tmp->getHtmlId() . \'-errors\')'
                 . ($node->tokenizer->isNext() ? '->addAttributes(%node.array);' : ';')
-                . ' foreach($_input->getErrors() as $_e) $_el->create(%2.var)->setText($_e)',
+                . ' foreach($_tmp->getErrors() as $_e) $_tmp2->create(%2.var)->setText($_e)',
                 $name,
                 $tagName,
                 $childName
@@ -77,11 +101,11 @@ class NittroMacros extends MacroSet {
 
         if ($node->prefix) {
             $node->openingCode = '<?php ' . $prefix . ' ?>';
-            $node->attrCode = '<?php echo $_el->attributes(); ?>';
-            $node->innerContent = '<?php echo $_el->getHtml() ?>';
+            $node->attrCode = '<?php echo $_tmp2->attributes(); ?>';
+            $node->innerContent = '<?php echo $_tmp2->getHtml() ?>';
         } else {
             $node->replaced = true;
-            return $prefix . '; echo $_el';
+            return $prefix . '; echo $_tmp2';
         }
     }
 }
