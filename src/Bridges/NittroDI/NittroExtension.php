ded25cbcfb83a0db4b53a70afd3a7d7aad172a1b
Dan Kadera <me@subsonic.cz>
2019-05-09 11:24:34 +0200

    fix migration to Nette\Schema

--- src/Bridges/NittroDI/NittroExtension.php
+++ src/Bridges/NittroDI/NittroExtension.php
@@ -4,24 +4,21 @@ declare(strict_types=1);
 
 namespace Nittro\Bridges\NittroDI;
 
-use Nette;
 use Nette\Bridges\ApplicationLatte\ILatteFactory;
 use Nette\DI\CompilerExtension;
 use Nette\DI\Definitions\FactoryDefinition;
 use Nette\DI\Definitions\Statement;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 use Nittro\Bridges\NittroLatte\NittroMacros;
 use Nittro\Bridges\NittroLatte\NittroRuntime;
 
 
 class NittroExtension extends CompilerExtension {
 
-    public $defaults = [
-        'noconflict' => false,
-    ];
-
-    public function getConfigSchema() : Nette\Schema\Schema {
-        return Nette\Schema\Expect::structure([
-            'noconflict' => Nette\Schema\Expect::bool(false),
+    public function getConfigSchema() : Schema {
+        return Expect::structure([
+            'noconflict' => Expect::bool(false),
         ]);
     }
 
